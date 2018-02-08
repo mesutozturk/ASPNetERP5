1c4e323d826ac73b62a779fc1bda72dfb0ff951e
Mesut OZTURK <mesutoztrk@msn.com>
2018-02-08 16:03:40 +0300

    EmployeeList

---
+++ MVCFundamentals/Controllers/EmployeeController.cs
@@ -0,0 +1,22 @@
+﻿using MVCFundamentals.Models;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace MVCFundamentals.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public async Task<ActionResult> Index()
+        {
+            var db = new NorthwindEntities();
+            var model = await db.Employees
+                .OrderBy(x => x.FirstName)
+                .ThenBy(x => x.LastName)
+                .ToListAsync();
+
+            return View(model);
+        }
+    }
+}
