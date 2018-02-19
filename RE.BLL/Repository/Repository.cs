c9b3a41a145d18cf2aad12951eb7b313a969d2a3
Mesut OZTURK <mesutoztrk@msn.com>
2018-02-19 19:34:01 +0300

    Reverse Engineering EntityFramework CodeFirst

---
+++ RE.BLL/Repository/Repository.cs
@@ -0,0 +1,11 @@
+﻿using RE.Models.Entities;
+
+namespace RE.BLL.Repository
+{
+    public class ProductRepo : RepositoryBase<Product, int> { }
+    public class CategoryRepo : RepositoryBase<Category, int> { }
+    public class EmployeeRepo : RepositoryBase<Employee, int> { }
+    public class ShipperRepo : RepositoryBase<Shipper, int> { }
+    public class SupplierRepo : RepositoryBase<Supplier, int> { }
+    public class CustomerRepo : RepositoryBase<Customer, string> { }
+}
---
+++ RE.BLL/Repository/RepositoryBase.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RE.DAL;
+
+namespace RE.BLL.Repository
+{
+    public abstract class RepositoryBase<T, Tid> where T : class
+    {
+        protected internal static MyContext dbContext;
+
+        public virtual List<T> GetAll()
+        {
+            try
+            {
+                dbContext = new MyContext();
+                return dbContext.Set<T>().ToList();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public virtual T GetById(Tid id)
+        {
+            try
+            {
+                dbContext = new MyContext();
+                return dbContext.Set<T>().Find(id);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public virtual int Insert(T entity)
+        {
+            try
+            {
+                dbContext = dbContext ?? new MyContext();
+                dbContext.Set<T>().Add(entity);
+                return dbContext.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public virtual int Delete(T entity)
+        {
+            try
+            {
+                dbContext = dbContext ?? new MyContext();
+                dbContext.Set<T>().Remove(entity);
+                return dbContext.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public virtual int Update()
+        {
+            try
+            {
+                dbContext = dbContext ?? new MyContext();
+                return dbContext.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
