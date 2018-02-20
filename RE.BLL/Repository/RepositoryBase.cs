fdbc5dea3f5336a8ad7a390c4babc5a39eb024f9
Mesut OZTURK <mesutoztrk@msn.com>
2018-02-20 16:01:49 +0300

    async

--- RE.BLL/Repository/RepositoryBase.cs
+++ RE.BLL/Repository/RepositoryBase.cs
@@ -1,13 +1,13 @@
-﻿using System;
+﻿using RE.DAL;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
-using RE.DAL;
 
 namespace RE.BLL.Repository
 {
-    public abstract class RepositoryBase<T, Tid> where T : class
+    public abstract class RepositoryBase<T, TId> : IDisposable where T : class
     {
         protected internal static MyContext dbContext;
 
@@ -23,8 +23,19 @@ namespace RE.BLL.Repository
                 throw ex;
             }
         }
-
-        public virtual T GetById(Tid id)
+        public virtual async Task<List<T>> GetAllAsync()
+        {
+            try
+            {
+                dbContext = new MyContext();
+                return await dbContext.Set<T>().ToListAsync();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        public virtual T GetById(TId id)
         {
             try
             {
@@ -36,7 +47,18 @@ namespace RE.BLL.Repository
                 throw ex;
             }
         }
-
+        public virtual async Task<T> GetByIdAsync(TId id)
+        {
+            try
+            {
+                dbContext = new MyContext();
+                return await dbContext.Set<T>().FindAsync(id);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
         public virtual int Insert(T entity)
         {
             try
@@ -50,7 +72,19 @@ namespace RE.BLL.Repository
                 throw ex;
             }
         }
-
+        public virtual async Task<int> InsertAsync(T entity)
+        {
+            try
+            {
+                dbContext = dbContext ?? new MyContext();
+                dbContext.Set<T>().Add(entity);
+                return await dbContext.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
         public virtual int Delete(T entity)
         {
             try
@@ -64,7 +98,19 @@ namespace RE.BLL.Repository
                 throw ex;
             }
         }
-
+        public virtual async Task<int> DeleteAsync(T entity)
+        {
+            try
+            {
+                dbContext = dbContext ?? new MyContext();
+                dbContext.Set<T>().Remove(entity);
+                return await dbContext.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
         public virtual int Update()
         {
             try
@@ -76,6 +122,48 @@ namespace RE.BLL.Repository
             {
                 throw ex;
             }
+        }
+        public virtual async Task<int> UpdateAsync()
+        {
+            try
+            {
+                dbContext = dbContext ?? new MyContext();
+                return await dbContext.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        public virtual IQueryable<T> Queryable()
+        {
+            try
+            {
+                dbContext = dbContext ?? new MyContext();
+                return dbContext.Set<T>().AsQueryable();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        public virtual ParallelQuery<T> Parallel()
+        {
+            try
+            {
+                dbContext = dbContext ?? new MyContext();
+                return dbContext.Set<T>().AsParallel();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        public void Dispose()
+        {
+            dbContext.Dispose();
+            GC.SuppressFinalize(this);
+            dbContext = null;
         }
     }
 }
