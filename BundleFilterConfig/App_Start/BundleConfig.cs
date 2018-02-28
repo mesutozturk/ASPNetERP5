cec2980df8223df129dbd92ebbc0e7e5cd9eca2c
Mesut OZTURK <mesutoztrk@msn.com>
2018-02-28 19:12:52 +0300

    bundle & filter config

---
+++ BundleFilterConfig/App_Start/BundleConfig.cs
@@ -0,0 +1,29 @@
+﻿using System.Web.Optimization;
+
+namespace BundleFilterConfig
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundle(BundleCollection bundles)
+        {
+            BundleTable.EnableOptimizations = true;
+            bundles.Add(new StyleBundle("~/bundles/styles").Include(
+                "~/Content/bootstrap.css",
+                "~/Content/bootstrap-grid.css",
+                "~/Content/bootstrap-reboot.css",
+                "~/Content/themes/base/all.css",
+                "~/Content/site.css"
+                ));
+
+            bundles.Add(new ScriptBundle("~/bundles/js").Include(
+                "~/Scripts/jquery-3.3.1.js",
+                "~/Scripts/jquery-ui-1.12.1.js",
+                "~/Scripts/bootstrap.js",
+                "~/Scripts/jquery.validate.js",
+                "~/Scripts/moment.js",
+                "~/Scripts/popper.js",
+                "~/Scripts/app.js"
+                ));
+        }
+    }
+}
---
+++ BundleFilterConfig/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using BundleFilterConfig.Services;
+using System.Web.Mvc;
+
+namespace BundleFilterConfig.Controllers
+{
+    [HandleError(View = "Error")]
+    [ExceptionHandlerFilter]
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
