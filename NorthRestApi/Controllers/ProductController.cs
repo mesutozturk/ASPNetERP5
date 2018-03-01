45ac2234bbdd254b7079a464257b997ef819c5bc
Mesut OZTURK <mesutoztrk@msn.com>
2018-03-01 19:22:51 +0300

    custom route config

--- NorthRestApi/Controllers/ProductController.cs
+++ NorthRestApi/Controllers/ProductController.cs
@@ -1,14 +1,144 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using NorthRestApi.Models;
+using System;
 using System.Linq;
-using System.Net;
-using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web.Http;
+using NorthRestApi.Models.ViewModels;
 
 namespace NorthRestApi.Controllers
 {
+    [RoutePrefix("api/urun")]
     public class ProductController : ApiController
     {
+        [HttpGet]
+        [Route("")]
+        public ResponseData GetAll()
+        {
+            try
+            {
+                var db = new NorthwindEntities();
+                var urunler = db.Products.Select(x => new
+                {
+                    x.CategoryID,
+                    x.Category.CategoryName,
+                    x.ProductID,
+                    x.ProductName,
+                    x.UnitPrice,
+                    TotalOrder = x.Order_Details.Count
+                }).ToList();
+                return new ResponseData()
+                {
+                    Success = true,
+                    Data = urunler
+                };
+            }
+            catch (Exception ex)
+            {
+                return new ResponseData()
+                {
+                    Success = false,
+                    Message = ex.Message
+                };
+            }
+        }
+        [Route("getir/{id:int:min(1)}")]
+        public ResponseData GetById(int id)
+        {
+            try
+            {
+                var db = new NorthwindEntities();
+                var urun = db.Products.Find(id);
+                if (urun == null)
+                    return new ResponseData()
+                    {
+                        Message = "Ürün bulunamadı",
+                        Success = false
+                    };
+                return new ResponseData()
+                {
+                    Success = true,
+                    Data = new
+                    {
+                        urun.CategoryID,
+                        urun.Category.CategoryName,
+                        urun.ProductID,
+                        urun.ProductName,
+                        urun.UnitPrice,
+                        TotalOrder = urun.Order_Details.Count
+                    }
+                };
+            }
+            catch (Exception ex)
+            {
+                return new ResponseData()
+                {
+                    Success = false,
+                    Message = ex.Message
+                };
+            }
+        }
 
+        [Route("{cid:int:min(0)}")]
+        public ResponseData GetAllByCatId(int cid)
+        {
+            try
+            {
+                var db = new NorthwindEntities();
+                var urunler = db.Products
+                    .Where(x => x.CategoryID == cid)
+                    .Select(x => new
+                    {
+                        x.CategoryID,
+                        x.Category.CategoryName,
+                        x.ProductID,
+                        x.ProductName,
+                        x.UnitPrice,
+                        TotalOrder = x.Order_Details.Count
+                    }).ToList();
+                return new ResponseData()
+                {
+                    Success = true,
+                    Data = urunler
+                };
+            }
+            catch (Exception ex)
+            {
+                return new ResponseData()
+                {
+                    Success = false,
+                    Message = ex.Message
+                };
+            }
+        }
+
+        [HttpPost]
+        [Route("ekle")]
+        public async Task<ResponseData> Insert([FromBody] ProductViewModel value)
+        {
+            try
+            {
+                var db = new NorthwindEntities();
+                db.Products.Add(new Product()
+                {
+                    CategoryID = value.CategoryId,
+                    ProductName = value.ProductName,
+                    UnitPrice = value.UnitPrice
+                });
+                await db.SaveChangesAsync();
+                return new ResponseData()
+                {
+                    Success = true,
+                    Message = $"{value.ProductName} isimli ürün ekleme işlemi başarılı"
+                };
+            }
+            catch (Exception ex)
+            {
+                return new ResponseData()
+                {
+                    Success = false,
+                    Message = ex.Message
+                };
+            }
+        }
     }
 }
---
+++ NorthRestApi/Models/ViewModels/ProductViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NorthRestApi.Models.ViewModels
+{
+    public class ProductViewModel
+    {
+        public int? CategoryId { get; set; }
+        [Required]
+        public string ProductName { get; set; }
+        public decimal UnitPrice { get; set; }
+    }
+}
